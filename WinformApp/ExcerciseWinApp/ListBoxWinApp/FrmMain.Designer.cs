f7d5649c22cbd711d162cecba43badc771f886cc
hyojin-park24 <gygy332@naver.com>
2021-03-10 12:22:17 +0900

    C# 윈폼 [ExcerciseWinApp]

---
+++ WinformApp/ExcerciseWinApp/ListBoxWinApp/FrmMain.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace ListBoxWinApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LsbGdpCountry = new System.Windows.Forms.ListBox();
+            this.LsbGoodCity = new System.Windows.Forms.ListBox();
+            this.LsbHappyCountry = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtIndexGdp = new System.Windows.Forms.TextBox();
+            this.TxtIndexGood = new System.Windows.Forms.TextBox();
+            this.TxtIndexHappy = new System.Windows.Forms.TextBox();
+            this.TxtItemGdp = new System.Windows.Forms.TextBox();
+            this.TxtItemGood = new System.Windows.Forms.TextBox();
+            this.TxtItemHappy = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnInit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LsbGdpCountry
+            // 
+            this.LsbGdpCountry.FormattingEnabled = true;
+            this.LsbGdpCountry.ItemHeight = 12;
+            this.LsbGdpCountry.Items.AddRange(new object[] {
+            "미국",
+            "러시아",
+            "중국",
+            "독일",
+            "프랑스",
+            "일본",
+            "이스라엘",
+            "사우디아라비아",
+            "UAE",
+            "한국",
+            "영국",
+            "스웨덴 "});
+            this.LsbGdpCountry.Location = new System.Drawing.Point(122, 77);
+            this.LsbGdpCountry.Name = "LsbGdpCountry";
+            this.LsbGdpCountry.Size = new System.Drawing.Size(151, 196);
+            this.LsbGdpCountry.TabIndex = 0;
+            this.LsbGdpCountry.SelectedIndexChanged += new System.EventHandler(this.LsbGdpCountry_SelectedIndexChanged);
+            // 
+            // LsbGoodCity
+            // 
+            this.LsbGoodCity.FormattingEnabled = true;
+            this.LsbGoodCity.ItemHeight = 12;
+            this.LsbGoodCity.Location = new System.Drawing.Point(315, 77);
+            this.LsbGoodCity.Name = "LsbGoodCity";
+            this.LsbGoodCity.Size = new System.Drawing.Size(151, 196);
+            this.LsbGoodCity.TabIndex = 3;
+            this.LsbGoodCity.SelectedIndexChanged += new System.EventHandler(this.LsbGoodCity_SelectedIndexChanged);
+            // 
+            // LsbHappyCountry
+            // 
+            this.LsbHappyCountry.FormattingEnabled = true;
+            this.LsbHappyCountry.ItemHeight = 12;
+            this.LsbHappyCountry.Location = new System.Drawing.Point(508, 77);
+            this.LsbHappyCountry.Name = "LsbHappyCountry";
+            this.LsbHappyCountry.Size = new System.Drawing.Size(151, 196);
+            this.LsbHappyCountry.TabIndex = 6;
+            this.LsbHappyCountry.SelectedIndexChanged += new System.EventHandler(this.LsbHappyCountry_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "GDP 국가 순위";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "살기좋은 도시";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(506, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "행복한 나라";
+            // 
+            // TxtIndexGdp
+            // 
+            this.TxtIndexGdp.Location = new System.Drawing.Point(122, 286);
+            this.TxtIndexGdp.Name = "TxtIndexGdp";
+            this.TxtIndexGdp.Size = new System.Drawing.Size(150, 21);
+            this.TxtIndexGdp.TabIndex = 1;
+            // 
+            // TxtIndexGood
+            // 
+            this.TxtIndexGood.Location = new System.Drawing.Point(315, 286);
+            this.TxtIndexGood.Name = "TxtIndexGood";
+            this.TxtIndexGood.Size = new System.Drawing.Size(150, 21);
+            this.TxtIndexGood.TabIndex = 4;
+            // 
+            // TxtIndexHappy
+            // 
+            this.TxtIndexHappy.Location = new System.Drawing.Point(508, 286);
+            this.TxtIndexHappy.Name = "TxtIndexHappy";
+            this.TxtIndexHappy.Size = new System.Drawing.Size(150, 21);
+            this.TxtIndexHappy.TabIndex = 7;
+            // 
+            // TxtItemGdp
+            // 
+            this.TxtItemGdp.Location = new System.Drawing.Point(122, 313);
+            this.TxtItemGdp.Name = "TxtItemGdp";
+            this.TxtItemGdp.Size = new System.Drawing.Size(150, 21);
+            this.TxtItemGdp.TabIndex = 2;
+            // 
+            // TxtItemGood
+            // 
+            this.TxtItemGood.Location = new System.Drawing.Point(315, 313);
+            this.TxtItemGood.Name = "TxtItemGood";
+            this.TxtItemGood.Size = new System.Drawing.Size(150, 21);
+            this.TxtItemGood.TabIndex = 5;
+            // 
+            // TxtItemHappy
+            // 
+            this.TxtItemHappy.Location = new System.Drawing.Point(508, 313);
+            this.TxtItemHappy.Name = "TxtItemHappy";
+            this.TxtItemHappy.Size = new System.Drawing.Size(151, 21);
+            this.TxtItemHappy.TabIndex = 8;
+            this.TxtItemHappy.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SelectedIndex : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 318);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "SelectedItem : ";
+            // 
+            // BtnInit
+            // 
+            this.BtnInit.Location = new System.Drawing.Point(508, 347);
+            this.BtnInit.Name = "BtnInit";
+            this.BtnInit.Size = new System.Drawing.Size(149, 30);
+            this.BtnInit.TabIndex = 9;
+            this.BtnInit.Text = "초기화";
+            this.BtnInit.UseVisualStyleBackColor = true;
+            this.BtnInit.Click += new System.EventHandler(this.BtnInit_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 393);
+            this.Controls.Add(this.BtnInit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtIndexHappy);
+            this.Controls.Add(this.TxtIndexGood);
+            this.Controls.Add(this.TxtItemHappy);
+            this.Controls.Add(this.TxtItemGood);
+            this.Controls.Add(this.TxtItemGdp);
+            this.Controls.Add(this.TxtIndexGdp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LsbHappyCountry);
+            this.Controls.Add(this.LsbGoodCity);
+            this.Controls.Add(this.LsbGdpCountry);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ListBox Test";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LsbGdpCountry;
+        private System.Windows.Forms.ListBox LsbGoodCity;
+        private System.Windows.Forms.ListBox LsbHappyCountry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtIndexGdp;
+        private System.Windows.Forms.TextBox TxtIndexGood;
+        private System.Windows.Forms.TextBox TxtIndexHappy;
+        private System.Windows.Forms.TextBox TxtItemGdp;
+        private System.Windows.Forms.TextBox TxtItemGood;
+        private System.Windows.Forms.TextBox TxtItemHappy;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnInit;
+    }
+}
+
---
+++ WinformApp/ExcerciseWinApp/ListBoxWinApp/FrmMain.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxWinApp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            //살기 좋은 도시 초기화 , 가장 기본적인 리스트박스 추가
+            LsbGoodCity.Items.Add("오스트리아,빈");
+            LsbGoodCity.Items.Add("호주, 맬버른");
+            LsbGoodCity.Items.Add("일본,오사카");
+            LsbGoodCity.Items.Add("캐나다,캘거리");
+            LsbGoodCity.Items.Add("호주, 시드니");
+            LsbGoodCity.Items.Add("캐나다, 밴쿠버");
+            LsbGoodCity.Items.Add("일본, 도쿄");
+            LsbGoodCity.Items.Add("캐나다, 토론토");
+            LsbGoodCity.Items.Add("덴마크, 코펜하겐");
+            LsbGoodCity.Items.Add("호주, 애들레이드");
+
+            List<string> lstCountry = new List<string>() { 
+                "미국","러시아","중국","독일","프랑스","일본","이스라엘",
+                "사우디아라비아","UAE","한국","영국","스웨덴"
+            };
+            LsbHappyCountry.DataSource = lstCountry;
+            LsbHappyCountry.SelectedIndex = -1; //리스트로 넣는 값에 대한 리스트박스 초기화
+
+
+
+        }
+        private void LsbGoodCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //선택된 값 확인 
+            //MessageBox.Show(sender.ToString());
+            var selItem = sender as ListBox;
+            // MessageBox.Show($"{selItem.SelectedIndex.ToString()} / {selItem.SelectedItem}");
+            TxtIndexGood.Text = selItem.SelectedIndex.ToString();
+            TxtItemGood.Text = selItem.SelectedItem == null ? string.Empty : selItem.SelectedItem.ToString();
+        }
+
+        private void LsbGdpCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //선택된 값 확인 
+            //MessageBox.Show(sender.ToString());
+            var selItem = sender as ListBox;
+            // MessageBox.Show($"{selItem.SelectedIndex.ToString()} / {selItem.SelectedItem}");
+            TxtIndexGdp.Text = selItem.SelectedIndex.ToString();
+            TxtItemGdp.Text = selItem.SelectedItem == null ? string.Empty : selItem.SelectedItem.ToString();
+        }
+
+        private void LsbHappyCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //선택된 값 확인 
+            //MessageBox.Show(sender.ToString());
+            var selItem = sender as ListBox;
+            // MessageBox.Show($"{selItem.SelectedIndex.ToString()} / {selItem.SelectedItem}");
+            TxtIndexHappy.Text = selItem.SelectedIndex.ToString();
+            TxtItemHappy.Text = selItem.SelectedItem == null ? string.Empty : selItem.SelectedItem.ToString();
+        }
+
+        private void BtnInit_Click(object sender, EventArgs e)
+        {
+            LsbGdpCountry.SelectedIndex = LsbGoodCity.SelectedIndex
+                = LsbHappyCountry.SelectedIndex = -1;
+        }
+    }
+}
