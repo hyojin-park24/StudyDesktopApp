9ca736d2874904f460fca244482cde832d53ec9c
hyojin-park24 <gygy332@naver.com>
2021-03-16 12:35:05 +0900

    오전 수업 커밋

---
+++ WinformApp/WinFormAdvancedBank/IoTSensorMonApp/FrmMain.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace IoTSensorMonApp
+{
+    public partial class FrmMain : Form
+    {
+        private double xCount = 200; //차트에 보이는 데이터 수
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            //ComboBox 초기화
+            foreach (var port in SerialPort.GetPortNames())
+            {
+                CboSerialPort.Items.Add(port);
+            }
+            CboSerialPort.Text = "Select Port";
+
+            // IoT장비에서 받을 값의 범위 
+            PrbPhotoResistor.Minimum = 0;
+            PrbPhotoResistor.Maximum = 1023;
+            PrbPhotoResistor.Value = 0;
+
+            // 차트모양 초기화 Line57
+            InitChartStyle();
+
+            // BtnDisplay 초기화 
+            BtnDisplay.BackColor = Color.DeepSkyBlue;
+            BtnDisplay.ForeColor = Color.WhiteSmoke;
+            BtnDisplay.Text = "NONE";
+            BtnDisplay.Font = new Font("맑은 고딕", 14, FontStyle.Bold);
+
+            //나머지 초기화
+            LblConnectTime.Text = "Connection Time :";
+            TxtSensorNum.TextAlign = HorizontalAlignment.Right;
+            TxtSensorNum.Text = "0";
+            BtnConnect.Enabled = BtnDisconnect.Enabled = false;
+        }
+
+        /// <summary>
+        /// 차트 초기 설정
+        /// </summary>
+
+        private void InitChartStyle()
+        {
+            ChtPhotoResistors.ChartAreas[0].BackColor = Color.Blue;
+            ChtPhotoResistors.ChartAreas[0].AxisX.Minimum = 0;
+            ChtPhotoResistors.ChartAreas[0].AxisX.Maximum = xCount;
+            ChtPhotoResistors.ChartAreas[0].AxisX.Interval = xCount / 4; //x의 간격
+            ChtPhotoResistors.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.WhiteSmoke;
+            ChtPhotoResistors.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+
+            ChtPhotoResistors.ChartAreas[0].AxisY.Minimum = 0;
+            ChtPhotoResistors.ChartAreas[0].AxisY.Maximum = 1024;
+            ChtPhotoResistors.ChartAreas[0].AxisY.Interval = xCount; 
+            ChtPhotoResistors.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.WhiteSmoke;
+            ChtPhotoResistors.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+
+            ChtPhotoResistors.ChartAreas[0].CursorX.AutoScroll = true;
+            ChtPhotoResistors.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            ChtPhotoResistors.ChartAreas[0].AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.SmallScroll;
+            ChtPhotoResistors.ChartAreas[0].AxisX.ScrollBar.ButtonColor = Color.LightSteelBlue;
+
+            ChtPhotoResistors.Series.Clear(); //Series 1 값 지움
+            ChtPhotoResistors.Series.Add("PhotoValue");
+            ChtPhotoResistors.Series[0].ChartType = SeriesChartType.Line;
+            ChtPhotoResistors.Series[0].Color = Color.LightCoral;
+            ChtPhotoResistors.Series[0].BorderWidth = 3;
+
+            // Series(범례) 글자 없애기
+            if (ChtPhotoResistors.Legends.Count > 0)
+            {
+                for (int i = 0; i < ChtPhotoResistors.Legends.Count; i++)
+                {
+                    ChtPhotoResistors.Legends.RemoveAt(i);
+                }
+            }
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            //TODO 나중에 실제 작업시 작성 
+        }
+
+        private void BtnDisconnect_Click(object sender, EventArgs e)
+        {
+            //TODO 나중에 실제 작업시 작성 
+        }
+
+        private Timer timerSimul = new Timer();
+        private Random randPhoto = new Random();
+        private bool IsSimulation = false;
+        private List<SensorData> sensors = new List<SensorData>(); //차트, 리스트박스에 계속 출력될 데이터
+
+        //시물레이션 시작
+        private void MnuBeginSimulation_Click(object sender, EventArgs e)
+        {
+            IsSimulation = true;
+            timerSimul.Enabled = true;
+            timerSimul.Interval = 1000; //1초
+            timerSimul.Tick += TimerSimul_Tick;
+            timerSimul.Start();
+        }
+
+        private void TimerSimul_Tick(object sender, EventArgs e)
+        {
+            int value = randPhoto.Next(1, 1023); //1~1023까지 사이의 값
+            ShowSensorValue(value.ToString());                                    
+        }
+
+
+        //센서값 화면 출력메서드
+        private void ShowSensorValue(string value)
+        {
+            int.TryParse(value, out int v);
+
+            var currentTime = DateTime.Now;
+            SensorData data = new SensorData(currentTime, v, IsSimulation);
+            sensors.Add(data);
+
+            //센서 값 갯수 출력
+            TxtSensorNum.Text = sensors.Count.ToString();
+            // 프로그레스바 현재값 실시간 출력
+            PrbPhotoResistor.Value = v;
+            // 리스트박스에 데이터 쌓기 
+            var item = $" {currentTime.ToString("yyyy-MM-dd HH:mm:ss")}\t{v}";
+            LsbPhotoResistors.Items.Add(item);
+            //스크롤 처리_리스트박스에서 가장 필요한것_WPF는 스크롤 작업이 너무 어렵고 복잡함
+            LsbPhotoResistors.SelectedIndex = LsbPhotoResistors.Items.Count - 1;
+
+            //차트에 데이터 출력
+            ChtPhotoResistors.Series[0].Points.Add(v);
+            
+        }
+
+        //시물레이션 끝
+        private void MnuEndSimulation_Click(object sender, EventArgs e)
+        {
+            IsSimulation = false;
+            timerSimul.Stop();
+        }
+
+
+        //종료버튼 클릭
+        private void MnuExit_Click(object sender, EventArgs e)
+        {
+            if (IsSimulation)
+            {
+                MessageBox.Show("시뮬레이션 멈춘 후 프로그램을 종료하세요", "종료",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ WinformApp/WinFormAdvancedBank/IoTSensorMonApp/SensorData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTSensorMonApp
+{
+   internal class SensorData
+    {   //속성 값
+        
+        public DateTime Current { get; set; } //현재 시간
+        public int Vlaue { get; set; } //센서값
+        public bool SimulFlag { get; set; } //시뮬레이션 여부
+
+        //생성자 만듦 : 속성값 + art+Enter
+        public SensorData(DateTime current, int vlaue, bool simulFlag)
+        {
+            Current = current;
+            Vlaue = vlaue;
+            SimulFlag = simulFlag;
+        }
+
+
+
+
+    }
+}
